ebf22dd5747af88b14c9c7fc4fa3fc2fde9fa8df
rayanry9 <rayanhldr@gmail.com>
2025-05-28 10:28:48 +0530

    feat: some more confs

--- init.lua
+++ init.lua
@@ -4,3 +4,21 @@ require("keymaps.telescope")
 require("lsp")
 
 vim.cmd.colorscheme "catppuccin-mocha"
+
+vim.opt.smarttab = false
+vim.opt.autoindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.linebreak = true
+vim.opt.scrolloff = 8
+vim.opt.wrap = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.history = 1000
+
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+
+vim.opt.wildmenu = true
+
+
